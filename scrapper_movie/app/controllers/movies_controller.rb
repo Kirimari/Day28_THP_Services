9e0811faeb62ff207df766c1e378713b209158a1
kirimari <ait.mus06@gmail.com>
2018-11-09 00:15:27 +0100

    scrapper movie

--- scrapper_movie/app/controllers/movies_controller.rb
+++ scrapper_movie/app/controllers/movies_controller.rb
@@ -1,11 +1,14 @@
 class MoviesController < ApplicationController
 
   def index
-
+    @film = Movie.all
   end
 
   def search
+    puts " ahahahhahah"
+    puts @film
     @film = SearchMovie.new(params[:q]).search
+    puts @film
   end
 
 end
